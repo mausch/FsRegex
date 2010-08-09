53302c4346ba97c264e8c05c0b863179a24a01da
mausch <mauricioscheffer@gmail.com>
2010-08-08 23:58:56 -0300

    moved impl to main project

---
+++ FsRegex/FsRegex.fs
@@ -0,0 +1,33 @@
+﻿module FsRegex
+
+open System
+open System.Text.RegularExpressions
+
+let join (s: string) (a: 'a seq) = String.Join(s, a)
+let sjoin a = join "" a
+let startLine = "^"
+let endLine = "$"
+let anyChar = "."
+let zeroOrMore a = a + "*"
+let oneOrMore a = a + "+"
+let zeroOrOne a = a + "?"
+let times (c: int) a = a + sprintf "{%d}" c
+let moreThanTimes (c: int) a = a + sprintf "{%d,}" c
+let timesRange (startTimes: int) (endTimes: int) a = a + sprintf "{%d,%d}" startTimes endTimes
+let digit = "\\d"
+let whiteSpace = "\\s"
+let word = "\\w"
+let wordBoundary = "\\b"
+let tab = "\\t"
+let literal = Regex.Escape
+let extEscape s =
+    let r = Regex.Escape s
+    r.Replace("-", "\\-")
+let oneOfChars (l: char seq) = sprintf "[%s]" (l |> Seq.map string |> Seq.map extEscape |> sjoin)
+let noneOfChars (l: char seq) = sprintf "[^%s]" (l |> Seq.map string |> Seq.map extEscape |> sjoin)
+let oneOf (l: string seq) = sprintf "(%s)" (l |> join "|") // does NOT assume literal elements!
+let extEscapeC = string >> extEscape
+let charRange startChar endChar = sprintf "[%s-%s]" (extEscapeC startChar) (extEscapeC endChar)
+let chars startChar endChar = sprintf "%s-%s" (extEscapeC startChar) (extEscapeC endChar)
+let range s = sprintf "[%s]" s
+let notRange s = sprintf "[^%s]" s
